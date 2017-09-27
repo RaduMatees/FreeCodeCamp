d7eb15bc6191ffaeac838f62fd532f6bf1db8974
Radu Matees <radumatees@gmail.com>
2017-09-27 16:15:27 +0300

    Creating Tribute page with bootstrap

---
+++ Basic-Front-End-Development-Projects/Tribute-Page/scripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("body").css("margin-top", '60px')
+  $("#grayed-out").css('color', 'grey')
+  $("h4").css('letter-spacing', '0.1em').css('line-height', '1.1em').css('margin-top', '20px')
+  $("footer").css('padding', '20px 0px 10px 0px').css('border-top', '1px solid #EEEEEE')
+  $(".bolded").css('font-weight', '900')
+})
