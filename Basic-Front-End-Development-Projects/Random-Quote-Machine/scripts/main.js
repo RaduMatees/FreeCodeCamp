fe22d83f7e6a6a08a762ece2a93ad0e00713eb5b
Radu Matees <radumatees@gmail.com>
2017-10-11 12:44:51 +0300

    Finalize style

--- Basic-Front-End-Development-Projects/Random-Quote-Machine/scripts/main.js
+++ Basic-Front-End-Development-Projects/Random-Quote-Machine/scripts/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
+  changeColors()
 })
+
+function changeColors() {
+  $('#new-quote-btn').on('click', function() {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    $('.my-body-background').css('background-color', randomColor)
+    $('.my-area').find('h2').css('color', randomColor)
+    $('.my-area').find('h4').css('color', randomColor)
+    $('#new-quote-btn').css('background-color', randomColor)
+  })
+}
