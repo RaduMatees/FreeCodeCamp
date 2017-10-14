195e6b2616c77addcf2a92cba52cccff469acffc
Radu Matees <radumatees@gmail.com>
2017-10-14 16:00:15 +0300

    Work done on Local Weather App

---
+++ Basic-Front-End-Development-Projects/Local-Weather-App/README.md
@@ -0,0 +1,2 @@
+Local Weather App ---
+View in browser --- http://htmlpreview.github.io/?https://github.com/RaduMatees/FreeCodeCamp/blob/master/Basic-Front-End-Development-Projects/Local-Weather-App/index.html
---
+++ Basic-Front-End-Development-Projects/Local-Weather-App/scripts/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  var latitude = 0
+  var longitude = 0
+
+  turnGPSOn()
+
+  function turnGPSOn() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        latitude = position.coords.latitude
+        longitude = position.coords.longitude
+        renderHTML()
+      })
+    }
+  }
+
+  function renderHTML() {
+    $.getJSON('https://fcc-weather-api.glitch.me/api/current?lon=' + longitude + '&lat=' + latitude, function(json) {
+      $('#location').html(json.name)
+      $('#temperature').html(Math.round(json.main.temp) + '°C')
+      var weather = json.weather[0].main
+      $('#weather').html(weather)
+      var renderIcon = json.weather[0].icon
+      renderIcon = "<img src='" + renderIcon + "'>"
+      $('#icon').html(renderIcon)
+      changeBackground(weather)
+    })
+  }
+
+  function changeBackground(weatherLike) {
+    if (weatherLike == 'Clouds') {
+      $('.my-img-container').addClass('img-clouds')
+    } else if (weather == 'Rain') {
+      $('.my-img-container').addClass('img-rainy')
+    } else if (weather == 'Clear') {
+      $('.my-img-container').addClass('img-clear')
+    } else if (weather == 'Mist') {
+      $('.my-img.container').addClass('img-mist')
+    } else if (weather == 'haze') {
+      $('.my-img.container').addClass('img-haze')
+    }
+  }
+
+})
