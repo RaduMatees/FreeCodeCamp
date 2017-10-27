3182a7c353cf0ea1b53099520ff5026be89c205c
Radu Matees <radumatees@gmail.com>
2017-10-27 14:36:49 +0300

    Connecting to the API and add customized li

--- Basic-Front-End-Development-Projects/Twitchtv-JSON-API/scripts/main.js
+++ Basic-Front-End-Development-Projects/Twitchtv-JSON-API/scripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  //executing code
+
   var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
   var url = ""
 
@@ -7,8 +9,15 @@ $(document).ready(function() {
     populatePage(i)
   }
 
+  $('#all-btn').on('click', function() {
+    ('.my-list-item').toggle()
+  })
+
+  // functions
+
   function populatePage(i) {
-    url = "https://wind-bow.gomix.me/twitch-api/users/" + streamers[i]
+
+    url = "https://wind-bow.gomix.me/twitch-api/streams/" + streamers[i] + "/?callback=?"
 
     $.ajax({
       type:"GET",
@@ -16,14 +25,47 @@ $(document).ready(function() {
       async: false,
       dataType: "jsonp",
       success: function(json) {
-        $('.my-list').append("<li class='my-list-item'><div class='my-round-avatars'> \
-        <img src='" + json.logo + "'></div><span class='my-list-names'>" + json.display_name + " \
-        </span></li>")
+        liveStream(json, i)
       },
       error: function(errorMessage){
         alert('Error')
       }
-    })
+    })// ajax call end
+  } // populatePage end
+
+
+  function liveStream(json, i) {
+    if (json.stream === null) {
+      offlineStream(i)
+    } else {
+      $('.my-list').append("<li class='my-list-item'><div class='my-round-avatars'> \
+      <img src='" + json.stream.channel.logo + "'></div><span class='my-list-names'>" + json.stream.channel.display_name + " \
+      </span></li>")
+      $('.my-list-item:last').wrap("<a href='https://go.twitch.tv/" + json.stream.channel.display_name + "' target=_blank></a>")
+      $('.my-list-item:last').append('<i class="fa fa-check my-check" aria-hidden="true"></i>')
+    }
+  }
+
+  function offlineStream(i) {
+
+    url2 = "https://wind-bow.gomix.me/twitch-api/users/" + streamers[i] + "/?callback=?"
+
+    $.ajax({
+      type:"GET",
+      url: url2,
+      async: false,
+      dataType: "jsonp",
+      success: function(json2) {
+        $('.my-list').append("<li class='my-list-item'><div class='my-round-avatars'> \
+        <img src='" + json2.logo + "'></div><span class='my-list-names'>" + json2.display_name + " \
+        </span></li>")
+        $('.my-list-item:last').wrap("<a href='https://go.twitch.tv/" + json2.display_name + "' target=_blank></a>")
+        $('.my-list-item:last').append('<i class="fa fa-times my-times" aria-hidden="true"></i>')
+      },
+      error: function(errorMessage){
+        alert('Error')
+      }
+    })// ajax call end
   }
 
 })
