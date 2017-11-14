273a84929493a0f57ba318c53fd3148909e95db6
Radu Matees <radumatees@gmail.com>
2017-11-14 13:42:30 +0200

    active buttons to change break and session length

--- Basic-Front-End-Development-Projects/Pomodoro-Clock/scripts/main.js
+++ Basic-Front-End-Development-Projects/Pomodoro-Clock/scripts/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
 
-  
+  $('.break .minus').on('click', function(){
+    var content = parseInt($('.break .modifiers-time').text())
+    $('.break .modifiers-time').html(content-1)
+    if (content <= 0){
+      $('.break .modifiers-time').html(0)
+    }
+  })
+
+  $('.break .plus').on('click', function(){
+    var content = parseInt($('.break .modifiers-time').text())
+    $('.break .modifiers-time').html(content+1)
+    if (content >= 10){
+      $('.break .modifiers-time').html(10)
+    }
+  })
+
+  $('.session .minus').on('click', function(){
+    var content = parseInt($('.session .modifiers-time').text())
+    $('.session .modifiers-time').html(content-5)
+    if (content <= 5) {
+      $('.session .modifiers-time').html(5)
+    }
+  })
+
+  $('.session .plus').on('click', function(){
+    var content = parseInt($('.session .modifiers-time').text())
+    $('.session .modifiers-time').html(content+5)
+    if (content >= 60) {
+      $('.session .modifiers-time').html(60)
+    }
+  })
 
 })
+
+function clock(){
+  setInterval(function(){
+    alert('bruh')
+  }, 3000)
+}
