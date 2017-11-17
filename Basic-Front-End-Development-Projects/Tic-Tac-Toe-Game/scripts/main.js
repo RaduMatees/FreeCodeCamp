7a7b06a7ba3e7ef223618897dbde694c6c9d6544
Radu Matees <radumatees@gmail.com>
2017-11-17 21:57:36 +0200

    Almost working 2 player game

--- Basic-Front-End-Development-Projects/Tic-Tac-Toe-Game/scripts/main.js
+++ Basic-Front-End-Development-Projects/Tic-Tac-Toe-Game/scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var player1weapon = 'X'
   var player2weapon = '0'
   var player1Turn = true
-  var positions = ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8']
+  var positions = ['#', '#', '#', '#', '#', '#', '#', '#', '#']
 
   drawSquares()
 
@@ -12,26 +12,53 @@ $(document).ready(function() {
       var clickedSquare = $(this).children('.X0')
       // if the square is free, do this
       if (clickedSquare.html() === ''){
-        alternatePlayers(clickedSquare)
+        // switch between turns
+        if (player1Turn){
+          clickedSquare.html(player1weapon)
+          player1Turn = false
+        } else {
+          clickedSquare.html(player2weapon)
+          player1Turn = true
+        }
       }
       var squareId = $(this).attr('id')
-      checkIfGameOver(squareId)
+      pushMoveToArray(squareId)
     })
   }
 
-  function alternatePlayers(square) {
-    // switch between turns
-    if (player1Turn){
-      square.html(player1weapon)
-      player1Turn = false
+  function pushMoveToArray(id) {
+    // push the newly created square content(X or 0) to the positions array at the correct index
+    var squareContent = $('#' + id).children('.X0').html()
+    index = id.substr(1)
+    positions[index] = squareContent
+    // who's player is the last move, so who wins?
+    if (player1Turn) {
+      var whoWins = '0'
     } else {
-      square.html(player2weapon)
-      player1Turn = true
+      var whoWins = 'X'
     }
+    checkIfGameOver(whoWins)
   }
 
-  function checkIfGameOver(id) {
-    console.log(id)
+  function checkIfGameOver(XOr0) {
+    // check possible winning strategies
+    if (positions[0] === XOr0 && positions[1] === XOr0 && positions[2] === XOr0){
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[3] === XOr0 && positions[4] === XOr0 && positions[5] === XOr0){
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[6] === XOr0 && positions[7] === XOr0 && positions[8] === XOr0) {
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[0] === XOr0 && positions[3] === XOr0 && positions[6] === XOr0) {
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[1] === XOr0 && positions[4] === XOr0 && positions[7] === XOr0) {
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[2] === XOr0 && positions[5] === XOr0 && positions[8] === XOr0) {
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[0] === XOr0 && positions[4] === XOr0 && positions[8] === XOr0) {
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    } else if (positions[2] === XOr0 && positions[4] === XOr0 && positions[6] === XOr0) {
+      $('#result').html('Player ' + XOr0 + ' wins').css('visibility', 'visible')
+    }
   }
 
 })
